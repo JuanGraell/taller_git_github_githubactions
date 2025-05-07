4c777cf59ef62c89f31e6bd317ff43e28b262472
JuanGraell <jgraell@unal.edu.co>
2025-05-07 14:00:17 -0500

    feat: Integrar flujo completo de ejecución en main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from estudiantes.registro import cargar_estudiantes, mostrar_tabla, calcular_promedio, mostrar_promedio
 
 def main():
-    # TODO: integrar flujo completo
-    pass
+    ruta = "estudiantes.csv"
+    estudiantes = cargar_estudiantes(ruta)
+    mostrar_tabla(estudiantes)
+    promedio = calcular_promedio(estudiantes)
+    mostrar_promedio(promedio)
 
 if __name__ == "__main__":
     main()
